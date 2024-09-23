861b0819275333460d6da66ae9ce0ab4add4cf21
Gutembergue Martins <82208925+gutembergue-martins@users.noreply.github.com>
2024-09-23 14:56:47 -0300

    Update setup.py
    atualização de versão para v2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing-Desafio_DIO_2024",
+    name="image_processing-Desafio_DIO_2024_v2",
     version="0.0.1",
     author="Gutembergue",
     author_email="gutemberguemartins@gmail.com",
